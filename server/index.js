535d3e8b839cc0fe6029b87eb41201af4b3ec2f8
Jorge Luis Serna <jorgelserve@icloud.com>
2018-05-01 13:12:10 -0500

    fetch ready

--- server/index.js
+++ server/index.js
@@ -1,18 +1,73 @@
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const bodyParser = require('body-parser')
 const express = require('express')
+const multer = require('multer')
 const helmet = require('helmet')
 const pug = require('pug')
-const session = require('cookie-session');
 
+
+const upload = multer() 
 const app = express()
 
 app.disable('x-powered-by')
 
+
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) 
+app.use(upload.array())
+app.use(cookieParser())
+app.use(session({
+	secret: "cookie_secret",
+	resave: false,
+	saveUninitialized: false,
+	secure: false,
+	maxAge: null 
+}))
+
+
 app.use(helmet())
 app.use(express.static('./public'))
+
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
+app.get('/', checkSignIn, (req, res) => {
 	res.render('index')
+})
+
+var Users = [{
+	email: 'jorgelserve@icloud.com',
+	password: '1710jo'
+}]
+
+app.post('/signin', (req, res) => {
+	if(!req.body.email || !req.body.password) {
+		res.send("Invalid details!")
+	} else {
+		Users.filter(function(user) {
+			if(user.email === req.body.email) {
+				res.send({res: 'User Already exists'})
+				// res.send('signup', {
+				// 	message: "User Already Exists! Login or choose another user id"
+				// })
+			}
+		})
+		
+		Users.push({
+			email: req.body.email,
+			password: req.body.password
+		})
+
+		req.session.user = {
+			email: req.body.email,
+			password: req.body.password
+		}
+
+		res.redirect('/')
+	}
+	res.end()
 })
 
 app.get('/signin', (req, res) => {
@@ -30,3 +85,15 @@ app.get('/api', (req, res) => {
 var port = process.env.PORT || 3000
 
 app.listen(port, console.log(`listening in port ${port}\n\t\x1b[36mhttp://localhost:${port} \x1b[0m`))
+
+
+
+function checkSignIn(req, res, next){
+	if(req.session.user){
+		next();     //If session exists, proceed to page
+	} else {
+		var err = new Error("Not logged in!");
+		console.log(req.session.user);
+		next(err);  //Error, trying to access unauthorized page!
+	}
+}
--- src/signin/template.js
+++ src/signin/template.js
@@ -8,19 +8,44 @@ module.exports = yo`<div class="container">
 
 			<h1 class="agroweather hide-on-small-only">AGROWEATHER</h1>
 			<h4 class="agroweather hide-on-med-and-up">AGROWEATHER</h4>
-			<form>
+			<form onsubmit=${onsubmit}>
 				<div class="input-field col s12 m6 offset-m3 l6 offset-l3">
-					<input id="email" type="email" class="validate">
+					<input id="email" type="email" class="validate" name="email">
 					<label for="email">Email</label>
 				</div>
 				<div class="input-field col s12 m6 offset-m3 l6 offset-l3">
-					<input id="password" type="password" class="validate">
+					<input id="password" type="password" class="validate" name="password">
 					<label for="password">Password</label>
 				</div>
 				<button type="submit" name="action" class="btn waves-effect waves-light blue col s12 m6 offset-m3 l6 offset-l3">Iniciar sesión
 					<i class="material-icons right">send</i>
 				</button>
 			</form>
-		</div>					
+		</div>
 	</div>
 </div>`
+
+
+async function onsubmit(ev) {
+	ev.preventDefault()
+
+	let header = {
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		method: "POST",
+		body: JSON.stringify({
+				email: document.getElementById('email').value,
+				password: document.getElementById('password').value,
+			})
+	}
+
+	try {
+		var user = await fetch('/signin', header).then(res => res.json())
+	} catch (err) {
+		console.log(err)
+	}
+	console.log(user)
+	// console.log(ev)
+}
